bc325d8fe1f5f65291db7cca502886b3caa490d5
Dongkyuuuu <desktop@kakao.com>
2020-03-16 23:15:29 +0900

    #17 noti 주석 추가

--- src/components/modal/notice/index.js
+++ src/components/modal/notice/index.js
@@ -3,6 +3,7 @@ import moment from 'moment';
 export default {
   methods: {
     changeSet() {
+      // 현재 시간 저장(1일동안 공지 표시안됨)
       this.$store.dispatch(
         'check/changeDate',
         moment(new Date()).format('YYYY-MM-DD')
